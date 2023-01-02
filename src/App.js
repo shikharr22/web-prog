a9d8a67575566c17647cb0fc956d3be5ad00f82d
shikharr22 <shikharraizaday@gmail.com>
2023-01-02 15:10:37 +0530

    Initial commit of web-prog project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="navbar"></div>
+      <div className="tabs-display">
+        <div className="tabs">
+          <ul className="tabs-list">
+            <li className="tab-item">Home</li>
+            <li className="tab-item">Courses Offered</li>
+            <li className="tab-item">Placements</li>
+            <li className="tab-item">Faculty</li>
+          </ul>
+        </div>
+
+        <div className="display"></div>
+      </div>
     </div>
   );
 }
