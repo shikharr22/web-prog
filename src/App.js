82a42efcaf4b05ae4a64015760ba79055527de0a
shikharr22 <shikharraizaday@gmail.com>
2023-01-04 22:50:17 +0530

    added sections to the web page

--- src/App.js
+++ src/App.js
@@ -3,68 +3,87 @@ import Placements from "./Placements";
 import Home from "./Home";
 import Courses from "./Courses";
 import Faculty from "./Faculty";
+import Navbar from "./Navbar";
 import { useEffect, useState } from "react";
 
-function App() {
+//Navbar, Placements, Home,Courses, Faculty are react functional components
 
-  const [home,setHome]=useState(true);
-  const [placements,setPlacements]=useState(true);
-  const [courses,setCourses]=useState(true);
-  const [faculty,setFaculty]=useState(true);
+const App = () => {
+  // state variables to handle the change of tabs
+  const [home, setHome] = useState(true);
+  const [placements, setPlacements] = useState(true);
+  const [courses, setCourses] = useState(true);
+  const [faculty, setFaculty] = useState(true);
 
-  useEffect(()=>{
+  // useEffect used to initialise the state variables with respective values once the page loads
+  useEffect(() => {
     setHome(true);
     setPlacements(false);
     setCourses(false);
     setFaculty(false);
-  },[])
+  }, []);
 
-  const handleHome=()=>{
+  // Functions to handle the change in state variables
+
+  const handleHome = () => {
     setHome(true);
     setPlacements(false);
     setCourses(false);
     setFaculty(false);
-  }
+  };
 
-  const handleCourses=()=>{
+  const handleCourses = () => {
     setHome(false);
     setPlacements(false);
     setCourses(true);
     setFaculty(false);
-  }
-  const handlePlacement=()=>{
+  };
+  const handlePlacement = () => {
     setHome(false);
     setPlacements(true);
     setCourses(false);
     setFaculty(false);
-  }
-  const handleFaculty=()=>{
+  };
+  const handleFaculty = () => {
     setHome(false);
     setPlacements(false);
     setCourses(false);
     setFaculty(true);
-  }
+  };
+
   return (
     <div className="container">
-      <div className="navbar"></div>
-      <div className="tabs-display">
+      <div className="navbar">
+        <Navbar/>
+      </div>
+      <div className="section-display">
         <div className="tabs">
           <ul className="tabs-list">
-            <li className="tab-item" onClick={handleHome}>Home</li>
-            <li className="tab-item" onClick={handleCourses}>Courses </li>
-            <li className="tab-item" onClick={handlePlacement}>Placements</li>
-            <li className="tab-item" onClick={handleFaculty}>Faculty</li>
+            {/* unordered list used to display the tabs */}
+            <li className="tab-item" onClick={handleHome}>
+              Home
+            </li>
+            <li className="tab-item" onClick={handleCourses}>
+              Courses{" "}
+            </li>
+            <li className="tab-item" onClick={handlePlacement}>
+              Placements
+            </li>
+            <li className="tab-item" onClick={handleFaculty}>
+              Faculty
+            </li>
           </ul>
         </div>
         <div className="display">
-          {home?<Home/>:''}
-          {placements?<Placements/>:''}
-          {courses?<Courses/>:''}
-          {faculty?<Faculty/>:''}
+          {/* ternary if statements used to display the tabs based on selection by user */}
+          {home ? <Home /> : ""}
+          {placements ? <Placements /> : ""}
+          {courses ? <Courses /> : ""}
+          {faculty ? <Faculty /> : ""}
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <div class="navbar-container">
+        <img style={{width:"2rem",height:"2rem" }} />
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
